75610482b3da68cce4eece50ac6e88e8d2427ada
Alesia Kuptsova <alesya.kuptsova@tut.by>
2020-10-17 00:13:25 +0200

    feat: implement a check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var brackets = {};
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        var openingBracket = bracketsConfig[i][0];
+        var closingBracket = bracketsConfig[i][1];
+        brackets[openingBracket] = closingBracket;
+    }
+    var stack = new Array();
+    for (var i = 0; i < str.length; i++) {
+        var valid = true;
+        if (stack.length === 0) {
+            valid = false;
+        } else {
+            var openingBracket = stack[stack.length-1];
+            var closingBracket = brackets[openingBracket];
+            if (str[i] === closingBracket ) {
+                stack.pop();
+            } else {
+                valid = false;
+            }
+        }
+        if(!valid) {
+            if(brackets[str[i]] !== undefined) {
+                stack.push(str[i]);
+            }else {
+                return false;
+            }
+        }
+    }
+    if ( stack.length > 0) {
+        return false;
+    }
+    return true;
 }
